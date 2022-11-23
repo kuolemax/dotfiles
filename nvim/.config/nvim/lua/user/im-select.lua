4878381f16874d9d8f4cebab4823386ab3c7b3ff
kuolemax <kuolemax@qq.com>
2022-11-23 11:18:04 +0800

    add im-select & upadte ranger image preview

---
+++ nvim/.config/nvim/lua/user/im-select.lua
@@ -0,0 +1,8 @@
+-- 设置 im-select 路径
+vim.g.im_select_command = "/opt/homebrew/bin/im-select"
+
+-- normal 模式输入法
+vim.g.im_select_default = "com.apple.keylayout.US"
+
+-- 在不同窗口/应用程序之间切换输入法, 设置为0
+vim.g.im_select_enable = 0
