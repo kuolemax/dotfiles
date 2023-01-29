2e14a61c39a408928f3e14278298fa8ffd73cb01
kuolemax <kuolemax@qq.com>
2023-01-29 10:11:35 +0800

    remove im-select

--- nvim/.config/nvim/lua/user/autocmd.lua
+++ nvim/.config/nvim/lua/user/autocmd.lua
@@ -4,3 +4,23 @@ vim.cmd [[
   \   exe "normal! g'\"" | 
   \ endif
 ]]
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+local os = vim.fn.has('macunix')
+
+if os == 1 then
+  autocmd("VimEnter", {
+    pattern = "*",
+    command = 'silent exec "!(/opt/homebrew/bin/im-select com.apple.keylayout.ABC)"',
+    group = cursorGrp,
+  })
+
+  autocmd("InsertLeave", {
+    pattern = "*",
+    command = 'silent exec "!(/opt/homebrew/bin/im-select com.apple.keylayout.ABC)"',
+    group = cursorGrp,
+  })
+end
+
